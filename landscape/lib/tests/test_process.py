0929e17259e06bfd446449d503f41e034ce57066
Kevin McDermott <kevin.mcdermott@canonical.com>
2008-06-20 17:57:37 +0100

    Failing test for the flooring of the CPU.

--- landscape/lib/tests/test_process.py
+++ landscape/lib/tests/test_process.py
@@ -17,8 +17,8 @@ class UptimeTest(LandscapeTest):
 class CalculatePCPUTest(unittest.TestCase):
 
     def test_calculate_pcpu_real_data(self):
-        self.assertEquals(calculate_pcpu(51286, 5000, 4, 9, 19000.07, 9281.0,
-                                         100), 3.0)
+        self.assertEquals(
+            calculate_pcpu(51286, 5000, 4, 9, 19000.07, 9281.0, 100), 3.0)
 
     def test_calculate_pcpu(self):
         """
@@ -28,9 +28,20 @@ class CalculatePCPUTest(unittest.TestCase):
         This should be cpu utilisation of 20%
         """
         self.assertEquals(calculate_pcpu(8000, 2000, 0, 0, 1000, 50000, 100),
-                                         20.0)
+                          20.0)
 
     def test_calculate_pcpu_capped(self):
+        """
+        This calculates the pcpu based on 100000 jiffies allocated to a process
+        over 50000 jiffies.
+
+        This should be cpu utilisation of 200% but capped at 99% CPU
+        utilisation.
+        """
+        self.assertEquals(calculate_pcpu(98000, 2000, 0, 0, 1000, 50000, 100),
+                          99.0)
+
+    def test_calculate_pcpu_floored(self):
         """
         This calculates the pcpu based on 100000 jiffies allocated to a process
         over 50000 jiffies.
@@ -38,5 +49,4 @@ class CalculatePCPUTest(unittest.TestCase):
         This should be cpu utilisation of 99% as it's capped at 99% CPU
         utilisation.
         """
-        self.assertEquals(calculate_pcpu(98000, 2000, 0, 0, 1000, 50000, 100),
-                                         99.0)
+        self.assertEquals(calculate_pcpu(1, 0, 0, 0, 50, 800, 10), 0.0)
