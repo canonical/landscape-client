6ec124213dcd8726086306358374cb46b6a34f68
Kevin McDermott <kevin@canonical.com>
2008-06-23 21:36:21 +0100

    Add more documentation to the test doc string.

--- landscape/lib/tests/test_process.py
+++ landscape/lib/tests/test_process.py
@@ -7,6 +7,24 @@ from landscape.lib.process import calculate_pcpu
 
 class CalculatePCPUTest(unittest.TestCase):
 
+    """
+    calculate_pcpu is lifted directly from procps/ps/output.c (it's called
+    "pcpu" in there).
+
+    What it actually does is...
+
+    The result is "number of jiffies allocated to the process / number of
+    jiffies the process has been running".
+
+    How the jiffies are allocated to the process is CPU agnostic, and my
+    reading of the percentage capping is to prevent something like...
+
+    jiffies allocated on CPU #1 600, jiffies allocated on CPU #2 600 = 1200
+    Jiffies allocated to a process that's only been running for 1000 jiffies
+
+    So, that would look wrong, but is entirely plausible.
+    """
+
     def test_calculate_pcpu_real_data(self):
         self.assertEquals(
             calculate_pcpu(51286, 5000, 4, 9, 19000.07, 9281.0, 100), 3.0)
