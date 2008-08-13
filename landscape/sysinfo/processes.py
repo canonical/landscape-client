f99df3c9535ccfc7c2b4e04266c56edc5c169a8a
Christopher Armstrong <radix@twistedmatrix.com>
2008-08-13 10:32:17 -0400

    A processes plugin which displays the number of processes and the number of zombie processes. - with tests - this looks bigger than it is because I moved   test_activeprocessinfo.SampleDataBuilder to helpers.ProcessDataBuilder.

---
+++ landscape/sysinfo/processes.py
@@ -0,0 +1,35 @@
+import os
+
+from twisted.internet.defer import succeed
+
+from landscape.lib.process import ProcessInformation
+
+
+class Processes(object):
+
+    def __init__(self, proc_dir="/proc"):
+        self._proc_dir = proc_dir
+
+    def register(self, sysinfo):
+        self._sysinfo = sysinfo
+
+    def run(self):
+        num_processes = 0
+        num_zombies = 0
+        for fn in os.listdir(self._proc_dir):
+            try:
+                pid = int(fn)
+                num_processes += 1
+            except ValueError:
+                continue
+            info = ProcessInformation(proc_dir=self._proc_dir)
+            if info.get_process_info(pid)["state"] == "Z":
+                num_zombies += 1
+        if num_zombies:
+            if num_zombies == 1:
+                msg = "There is 1 zombie process."
+            else:
+                msg = "There are %d zombie processes." % (num_zombies,)
+            self._sysinfo.add_note(msg)
+        self._sysinfo.add_header("Processes", str(num_processes))
+        return succeed(None)
---
+++ landscape/sysinfo/tests/test_processes.py
@@ -0,0 +1,51 @@
+from twisted.internet.defer import Deferred
+
+from landscape.sysinfo.sysinfo import SysInfoPluginRegistry
+from landscape.sysinfo.processes import Processes
+from landscape.tests.helpers import LandscapeTest, ProcessDataBuilder
+
+
+class ProcessesTest(LandscapeTest):
+
+    def setUp(self):
+        super(ProcessesTest, self).setUp()
+        self.fake_proc = self.make_dir()
+        self.processes = Processes(proc_dir=self.fake_proc)
+        self.sysinfo = SysInfoPluginRegistry()
+        self.sysinfo.add(self.processes)
+        self.builder = ProcessDataBuilder(self.fake_proc)
+
+    def test_run_returns_succeeded_deferred(self):
+        result = self.processes.run()
+        self.assertTrue(isinstance(result, Deferred))
+        called = []
+        def callback(result):
+            called.append(True)
+        result.addCallback(callback)
+        self.assertTrue(called)
+
+    def test_number_of_processes(self):
+        """The number of processes is added as a header."""
+        for i in range(3):
+            self.builder.create_data(i, self.builder.RUNNING, uid=0, gid=0,
+                                     process_name="foo%d" % (i,))
+        self.processes.run()
+        self.assertEquals(self.sysinfo.get_headers(),
+                          [("Processes", "3")])
+
+    def test_number_of_zombies(self):
+        """The number of zombies is added as a note."""
+        self.builder.create_data(99, self.builder.ZOMBIE, uid=0, gid=0,
+                                 process_name="ZOMBERS")
+        self.processes.run()
+        self.assertEquals(self.sysinfo.get_notes(),
+                          ["There is 1 zombie process."])
+
+    def test_multiple_zombies(self):
+        """Stupid English, and its plurality"""
+        for i in range(2):
+            self.builder.create_data(i, self.builder.ZOMBIE, uid=0, gid=0,
+                                     process_name="ZOMBERS%d" % (i,))
+        self.processes.run()
+        self.assertEquals(self.sysinfo.get_notes(),
+                          ["There are 2 zombie processes."])
