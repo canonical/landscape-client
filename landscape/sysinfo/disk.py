c7ae2e052d5b3011016964d34fef4ff75afc5033
Christopher Armstrong <radix@twistedmatrix.com>
2008-08-14 10:11:22 -0400

    - tests for disk plugin - only add a note when usage is above 85%, WITH TESTS - format megabytes nicely, with tests

--- landscape/sysinfo/disk.py
+++ landscape/sysinfo/disk.py
@@ -6,17 +6,30 @@ from twisted.internet.defer import succeed
 
 from landscape.monitor.mountinfo import MountInfo
 
+def format_megabytes(megabytes):
+    if megabytes >= 1024*1024:
+        return "%.2fTB" % (megabytes/(1024*1024))
+    elif megabytes >= 1024:
+        return "%.2fGB" % (megabytes/1024)
+    else:
+        return "%dMB" % (megabytes)
 
 class Disk(object):
+
+    def __init__(self, mounts_file, statvfs):
+        self._mounts_file = mounts_file
+        self._statvfs = statvfs
 
     def register(self, sysinfo):
         self._sysinfo = sysinfo
 
     def run(self):
-        mi = MountInfo()
+        mi = MountInfo(mounts_file=self._mounts_file, statvfs=self._statvfs)
         for info in mi._get_mount_info():
             total, free = mi._get_space(info["mount-point"])
-            used = (total - free) / total
-            self._sysinfo.add_note("%s is using %0.0f%% of %s"
-                                   % (info["mount-point"], used*100, total))
+            used = ((total - free) / total) * 100
+            if used >= 85:
+                self._sysinfo.add_note("%s is using %0.1f%% of %s"
+                                       % (info["mount-point"], used,
+                                          format_megabytes(total / 1024)))
         return succeed(None)
--- landscape/sysinfo/tests/test_disk.py
+++ landscape/sysinfo/tests/test_disk.py
@@ -1,15 +1,20 @@
 from twisted.internet.defer import Deferred
 
 from landscape.sysinfo.sysinfo import SysInfoPluginRegistry
-from landscape.sysinfo.disk import Disk
+from landscape.sysinfo.disk import Disk, format_megabytes
 from landscape.tests.helpers import LandscapeTest
 
+def mb(n): return n * 1024 * 1024
 
 class DiskTest(LandscapeTest):
 
     def setUp(self):
         super(DiskTest, self).setUp()
-        self.disk = Disk()
+        self.mount_file = self.make_path("")
+        self.stat_results = {}
+
+        self.disk = Disk(mounts_file=self.mount_file,
+                         statvfs=self.stat_results.get)
         self.sysinfo = SysInfoPluginRegistry()
         self.sysinfo.add(self.disk)
 
@@ -26,4 +31,55 @@ class DiskTest(LandscapeTest):
         self.disk.run()
         self.assertEquals(self.sysinfo.get_notes(), [])
 
-    
+    def test_over_85_percent(self):
+        """
+        When a filesystem is using more than 85% capacity, a note will be
+        displayed.
+        """
+        self.stat_results["/"] = (4096, 0, mb(1000), mb(150), 0, 0, 0, 0, 0)
+        f = open(self.mount_file, "w")
+        f.write("/dev/sda1 / rootfs rw 0 0\n")
+        f.close()
+
+        self.disk.run()
+        self.assertEquals(self.sysinfo.get_notes(),
+                          ["/ is using 85.0% of 3.91GB"])
+
+    def test_under_85_percent(self):
+        """No note is displayed for a filesystem using less than 85% capacity.
+        """
+        self.stat_results["/"] = (4096, 0, mb(1000), mb(151), 0, 0, 0, 0, 0)
+        f = open(self.mount_file, "w")
+        f.write("/dev/sda1 / rootfs rw 0 0\n")
+        f.close()
+
+        self.disk.run()
+        self.assertEquals(self.sysinfo.get_notes(), [])
+
+    def test_multiple_notes(self):
+        """
+        A note will be displayed for each filesystem using 85% or more capacity.
+        """
+        self.stat_results["/"] = (4096, 0, mb(1000), mb(150), 0, 0, 0, 0, 0)
+        self.stat_results["/use"] = (8192, 0, mb(2000), mb(200), 0, 0, 0, 0, 0)
+        self.stat_results["/emp"] = (12288, 0, mb(3000), mb(500), 0, 0, 0, 0, 0)
+
+        f = open(self.mount_file, "w")
+        f.write("""\
+/dev/sda1 / rootfs rw 0 0
+/dev/sda2 /use rootfs rw 0 0
+/dev/sda3 /emp rootfs rw 0 0
+""")
+        f.close()
+
+        self.disk.run()
+        self.assertEquals(self.sysinfo.get_notes(),
+                          ["/ is using 85.0% of 3.91GB",
+                           "/use is using 90.0% of 15.62GB"])
+
+    def test_format_megabytes(self):
+        self.assertEquals(format_megabytes(100), "100MB")
+        self.assertEquals(format_megabytes(1023), "1023MB")
+        self.assertEquals(format_megabytes(1024), "1.00GB")
+        self.assertEquals(format_megabytes(1024*1024-1), "1024.00GB")
+        self.assertEquals(format_megabytes(1024*1024), "1.00TB")
