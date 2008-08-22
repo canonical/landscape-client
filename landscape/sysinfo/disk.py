e43cb31676df13db107505ec9e298af16ce5c20e
Christopher Armstrong <radix@twistedmatrix.com>
2008-08-21 20:02:29 -0400

    - Don't show duplicate notes when two filesystems are mounted at the same path - Don't show almost-duplicate notes when the same device is mounted at two   paths - WITH TESTS!

--- landscape/sysinfo/disk.py
+++ landscape/sysinfo/disk.py
@@ -37,9 +37,19 @@ class Disk(object):
         self._sysinfo.add_header("Usage of " + main_info["mount-point"],
                                  usage(main_info))
 
-        for info in get_mount_info(self._mounts_file, self._statvfs):
+        seen_mounts = set()
+        seen_devices = set()
+        infos = list(get_mount_info(self._mounts_file, self._statvfs))
+        infos.sort(key=lambda i: i["mount-point"])
+        for info in infos:
             total = info["total-space"]
 
+            if info["mount-point"] in seen_mounts:
+                continue
+            seen_mounts.add(info["mount-point"]) 
+            if info["device"] in seen_devices:
+                continue
+            seen_devices.add(info["device"])
             if info["filesystem"] in ("udf", "iso9660"):
                 continue
             if total <= 0:
