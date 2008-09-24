1a48966bba9144c579a993939ae405607827367e
Christopher Armstrong <radix@twistedmatrix.com>
2008-09-24 17:08:03 -0400

    change the unknown package data timeout from 24h to 70m. plus a giant comment explaining the timeout.

--- landscape/package/changer.py
+++ landscape/package/changer.py
@@ -14,7 +14,21 @@ SUCCESS_RESULT = 1
 ERROR_RESULT = 100
 DEPENDENCY_ERROR_RESULT = 101
 
-UNKNOWN_PACKAGE_DATA_TIMEOUT = 24 * 60 * 60
+
+# The amount of time to wait while we have unknown package data before
+# reporting an error to the server in response to an operation.
+# The two common cases of this are:
+# 1.  The server requested an operation that we've decided requires some
+# dependencies, but we don't know the package ID of those dependencies.  It
+# should only take a bit more than 10 minutes for that to be resolved by the
+# package reporter.
+# 2.  We lost some package data, for example by a deb archive becoming
+# inaccessible for a while.  The earliest we can reasonably assume that to be
+# resolved is in 60 minutes, when the smart cronjob runs again.
+
+# So we'll give the problem one chance to resolve itself, by only waiting for
+# one run of smart update.
+UNKNOWN_PACKAGE_DATA_TIMEOUT = 70 * 60
 
 
 class UnknownPackageData(Exception):
