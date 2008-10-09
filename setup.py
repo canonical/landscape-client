a6a760ceda5ea2e1906e176753d81fec3434df73
Andreas Hasenack <andreas@canonical.com>
2008-10-09 14:50:38 -0300

    Updated files for landscape-client_1.0.23-0ubuntu0.6.06.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="Landscape Client",
-      version="AUTOPPA_VERSION(1.0.23-0ubuntu0.8.04)"[len("AUTOPPA_VERSION("):-1],
+      version="AUTOPPA_VERSION(1.0.23-0ubuntu0.6.06)"[len("AUTOPPA_VERSION("):-1],
       description="Landscape Client",
       author="Landscape Team",
       author_email="landscape-team@canonical.com",
