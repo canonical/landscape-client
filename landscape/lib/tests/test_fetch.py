bb85328e177ef86d1a16a93fc1622494d430dc79
Gustavo Niemeyer <gustavo@niemeyer.net>
2009-03-11 14:47:55 -0300

    Modified the fetch() function so that it raises HTTPCodeError if the server returns a non-200 code, rather than happily returning the body of the error page as if it were the actual wanted content.

--- landscape/lib/tests/test_fetch.py
+++ landscape/lib/tests/test_fetch.py
@@ -1,15 +1,21 @@
 import pycurl
 
-from landscape.lib.fetch import fetch, fetch_async
+from landscape.lib.fetch import fetch, fetch_async, HTTPCodeError
 from landscape.tests.helpers import LandscapeTest
 
 
 class CurlStub(object):
 
-    def __init__(self, result=None):
+    def __init__(self, result=None, http_code=200):
+        self.result = result
+        self._http_code = http_code
         self.options = {}
         self.performed = False
-        self.result = result
+
+    def getinfo(self, what):
+        if what == pycurl.HTTP_CODE:
+            return self._http_code
+        raise RuntimeError("Stub doesn't know about %d info" % what)
 
     def setopt(self, option, value):
         if self.performed:
@@ -93,6 +99,20 @@ class FetchTest(LandscapeTest):
                            pycurl.MAXREDIRS: 5,
                            pycurl.WRITEFUNCTION: Any(),
                            pycurl.HTTPHEADER: ["a: 1", "b: 2"]})
+
+    def test_non_200_result(self):
+        curl = CurlStub("result", http_code=404)
+        try:
+            fetch("http://example.com", curl=curl)
+        except HTTPCodeError, error:
+            self.assertEquals(error.http_code, 404)
+            self.assertEquals(error.body, "result")
+        else:
+            self.fail("HTTPCodeError not raised")
+
+    def test_error_repr(self):
+        self.assertEquals(repr(HTTPCodeError(501, "")),
+                          "Server returned HTTP code 501")
 
     def test_create_curl(self):
         curls = []
