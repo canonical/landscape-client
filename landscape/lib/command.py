73891b9c4288b88532fb24d15002e71a85ebc3f4
free.ekanayaka@canonical.com <>
2009-03-27 14:07:24 +0100

    Add docstring to CommandError and command.py

--- landscape/lib/command.py
+++ landscape/lib/command.py
@@ -1,7 +1,14 @@
+"""Shell commands execution."""
 import commands
 
 class CommandError(Exception):
+    """
+    Raised by L{run_command} in case of non-0 exit code.
 
+    @cvar command: The shell command that failed.
+    @cvar exit_status: Its non-zero exit status.
+    @cvar output: The command's output.
+    """
     def __init__(self, command, exit_status, output):
         self.command = command
         self.exit_status = exit_status
@@ -15,10 +22,13 @@ class CommandError(Exception):
         return "<CommandError command=<%s> exit_status=%d output=<%s>>" % (
             self.command, self.exit_status, self.output)
 
+
 def run_command(command):
     """
-    Execute a command in a shell and return the command's output. If the
-    command's exit status is not 0 a CommandError exception is raised.
+    Execute a command in a shell and return the command's output.
+
+    If the command's exit status is not 0 a L{CommandError} exception
+    is raised.
     """
     exit_status, output = commands.getstatusoutput(command)
     # shift down 8 bits to get shell-like exit codes
