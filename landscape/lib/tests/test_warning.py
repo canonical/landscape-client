a5b9102ac375dc94a53499417883e7ac92abe283
Christopher Armstrong <radix@twistedmatrix.com>
2009-04-09 10:32:45 -0400

    hide warnings!

---
+++ landscape/lib/tests/test_warning.py
@@ -0,0 +1,31 @@
+import warnings
+
+from twisted.trial.unittest import TestCase
+
+from landscape.lib.warning import hide_warnings
+
+
+class WarningTest(TestCase):
+    def setUp(self):
+        super(WarningTest, self).setUp()
+        self.orig_filters = warnings.filters[:]
+
+    def tearDown(self):
+        super(WarningTest, self).tearDown()
+        warnings.filters[:] = self.orig_filters
+
+    def test_hide_warnings(self):
+        hide_warnings()
+        filters = warnings.filters[:2]
+
+        # Warning filters are processed beginning to end, and the first filter
+        # which matches a particular warning is used.
+
+        # The frontmost should "default" (i.e. print) on UserWarnings
+        self.assertEquals(filters[0][0], "default")
+        self.assertEquals(filters[0][2], UserWarning)
+
+        # The one just behind that should indicate that we should ignore all
+        # other warnings.
+        self.assertEquals(filters[1][0], "ignore")
+        self.assertEquals(filters[1][2], Warning)
