7ecd982f0c581a8d0a0499fa6d55ae7d4d64bc44
Free Ekanayaka <free.ekanayaka@canonical.com>
2010-01-07 07:16:50 +0100

    Add a socket class variable to RemoteLandscapeComponentCreatorBase

--- landscape/amp.py
+++ landscape/amp.py
@@ -24,16 +24,20 @@ class LandscapeComponentProtocolFactory(MethodCallFactory):
         MethodCallFactory.__init__(self, reactor._reactor, component)
 
 
-class RemoteLandscapeComponentCreator(RemoteObjectCreator):
-    """Helper to create connections with a Landscape component."""
+class RemoteLandscapeComponentCreatorBase(RemoteObjectCreator):
+    """Helper to create connections with a Landscape component.
+
+    @cvar socket: The name of the socket to connect to, it must be set
+        by the subclasses.
+    """
 
     protocol = MethodCallProtocol
 
-    def __init__(self, reactor, config, name):
+    def __init__(self, reactor, config):
         """
         @param reactor: A L{TwistedReactor} object.
         @param config: A L{LandscapeConfiguration}.
-        @param name: The name of the Landscape service to connect to.
         """
-        socket = os.path.join(config.data_path, name + ".sock")
-        super(self.__class__, self).__init__(reactor._reactor, socket)
+        socket = os.path.join(config.data_path, self.socket + ".sock")
+        super(RemoteLandscapeComponentCreatorBase, self).__init__(
+            reactor._reactor, socket)
