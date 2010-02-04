90a9e7b14f9a8dcfbdf5f749adbfd0b0f0f0efb9
Jamu Kakar <jkakar@kakar.ca>
2010-02-04 12:38:49 -0800

    - Refactored the EucalyptusCloudManager plugin to use factories to   get a ServiceHub and EucalyptusInfo.  New tests provide fakes   using these factories.

--- landscape/manager/eucalyptus.py
+++ landscape/manager/eucalyptus.py
@@ -18,7 +18,7 @@ class EucalyptusInfo(object):
     def get_storage_controller_info(self):
         return self._tools._runTool("euca_conf", ["--list-scs"])
 
-    def get_node_info(self):
+    def get_node_controller_info(self):
         return self._tools._runTool("euca_conf", ["--list-nodes"])
 
 
@@ -28,6 +28,15 @@ class EucalyptusCloudManager(ManagerPlugin):
     plugin_name = "eucalyptus-manager"
     message_type = "eucalyptus-info"
     run_interval = 3600
+
+    def __init__(self, service_hub_factory=None, eucalyptus_info_factory=None):
+        super(EucalyptusCloudManager, self).__init__()
+        self._service_hub_factory = service_hub_factory
+        if self._service_hub_factory is None:
+            self._service_hub_factory = start_service_hub
+        self._eucalyptus_info_factory = eucalyptus_info_factory
+        if self._eucalyptus_info_factory is None:
+            self._eucalyptus_info_factory = get_eucalyptus_info
 
     def run(self):
         """Run the plugin.
@@ -41,35 +50,21 @@ class EucalyptusCloudManager(ManagerPlugin):
             running.
         """
         try:
-            service_hub = self._start_service_hub()
+            data_path = self.registry.config.data_path
+            service_hub = self._service_hub_factory(data_path)
         except:
             self.registry.remove(self)
             logging.info("Couldn't start service hub.  '%s' plugin has been "
                          "disabled." % self.message_type)
         else:
+            from imagestore.eucaservice import GetEucaInfo
+
             deferred = service_hub.addTask(GetEucaInfo("admin"))
             deferred.addCallback(self._get_message)
             deferred.addErrback(self._get_error_message)
             deferred.addCallback(self.registry.broker.send_message)
             deferred.addBoth(lambda ignored: service_hub.stop())
             return deferred
-
-    def _start_service_hub(self):
-        """Create and start a C{ServiceHub} to use when getting credentials.
-
-        @return: A running C{ServiceHub} with a C{EucaService} service that
-            can be used to retrieve credentials.
-        """
-        from twisted.internet import reactor
-        from imagestore.lib.service import ServiceHub
-        from imagestore.eucaservice import EucaService
-
-        data_path = self.registry.config.data_path
-        base_path = os.path.join(data_path, "eucalyptus")
-        service_hub = ServiceHub()
-        service_hub.addService(EucaService(reactor, base_path))
-        service_hub.start()
-        return service_hub
 
     def _get_message(self, credentials):
         """Create a message with information about a Eucalyptus cloud.
@@ -79,13 +74,11 @@ class EucalyptusCloudManager(ManagerPlugin):
         @return: A message with information about Eucalyptus to send to the
             server.
         """
-        from imagestore.eucaservice import EucaTools
-
-        tools = EucaTools(credentials)
-        info = EucalyptusInfo(tools)
+        info = self._eucalyptus_info_factory(credentials)
         walrus_info = info.get_walrus_info()
         cluster_controller_info = info.get_cluster_controller_info()
         storage_controller_info = info.get_storage_controller_info()
+        node_controller_info = info.get_node_controller_info()
         data = {"access_key": credentials.accessKey,
                 "secret_key": credentials.secretKey,
                 "private_key_path": credentials.privateKeyPath,
@@ -93,10 +86,11 @@ class EucalyptusCloudManager(ManagerPlugin):
                 "cloud_certificate_path": credentials.cloudCertificatePath,
                 "url_for_s3": credentials.urlForS3,
                 "url_for_ec2": credentials.urlForEC2}
-        return {"type": self.message_type, "credentials": data,
+        return {"type": self.message_type, "basic_info": data,
                 "walrus_info": walrus_info,
                 "cluster_controller_info": cluster_controller_info,
-                "storage_controller_info": storage_controller_info}
+                "storage_controller_info": storage_controller_info,
+                "node_controller_info": node_controller_info}
 
     def _get_error_message(self, failure):
         """Create an error message.
@@ -107,3 +101,31 @@ class EucalyptusCloudManager(ManagerPlugin):
         """
         error = failure.getBriefTraceback()
         return {"type": self.message_type, "error": error}
+
+
+def start_service_hub(self, data_path):
+    """Create and start a C{ServiceHub} to use when getting credentials.
+
+    @param data_path: The path to Landscape's data directory.
+    @return: A running C{ServiceHub} with a C{EucaService} service that
+        can be used to retrieve credentials.
+    """
+    from twisted.internet import reactor
+    from imagestore.lib.service import ServiceHub
+    from imagestore.eucaservice import EucaService
+
+    base_path = os.path.join(data_path, "eucalyptus")
+    service_hub = ServiceHub()
+    service_hub.addService(EucaService(reactor, base_path))
+    service_hub.start()
+    return service_hub
+
+
+def get_eucalyptus_info(self, credentials):
+    """Create a L{EucalyptusInfo} instance.
+
+    @param credentials: An C{imagestore.eucaservice.EucaInfo} instance.
+    @return: A L{EucalyptusInfo} instance.
+    """
+    from imagestore.eucaservice import EucaTools
+    return EucalyptusInfo(EucaTools(credentials))
--- landscape/manager/tests/test_eucalyptus.py
+++ landscape/manager/tests/test_eucalyptus.py
@@ -1,5 +1,68 @@
+from twisted.internet.defer import succeed
+
+from imagestore.eucaservice import FakeEucaInfo
+
 from landscape.manager.eucalyptus import EucalyptusCloudManager
 from landscape.tests.helpers import LandscapeTest, ManagerHelper
+
+
+fake_walrus_output = """\
+registered walruses:
+  walrus 10.0.1.113
+"""
+
+fake_cluster_controller_output = """\
+registered clusters:
+  dynamite 10.0.1.113
+"""
+
+fake_storage_controller_output = """\
+registered storage controllers:
+  dynamite 10.0.1.113
+"""
+
+fake_node_controller_output = """\
+registered nodes:
+  10.1.1.71  canyonedge   i-2DC5056F i-5DE5176D
+  10.1.1.72  canyonedge
+  10.1.1.73  canyonedge
+  10.1.1.74  canyonedge
+  10.1.1.75  canyonedge
+"""
+
+class FakeEucalyptusInfo(object):
+
+    def __init__(self, walrus_output=None, cluster_controller_output=None,
+                 storage_controller_output=None, node_controller_output=None):
+        self._walrus_output = walrus_output
+        self._cluster_controller_output = cluster_controller_output
+        self._storage_controller_output = storage_controller_output
+        self._node_controller_output = node_controller_output
+
+    def get_walrus_info(self):
+        return self._walrus_output
+
+    def get_cluster_controller_info(self):
+        return self._cluster_controller_output
+
+    def get_storage_controller_info(self):
+        return self._storage_controller_output
+
+    def get_node_controller_info(self):
+        return self._node_controller_output
+
+
+class FakeServiceHub(object):
+
+    def __init__(self, result):
+        self._result = result
+        self.stopped = 0
+
+    def addTask(self, task_handler):
+        return self._result
+
+    def stop(self):
+        self.stopped += 1
 
 
 class EucalyptusCloudManagerTest(LandscapeTest):
@@ -10,25 +73,73 @@ class EucalyptusCloudManagerTest(LandscapeTest):
         super(EucalyptusCloudManagerTest, self).setUp()
         message_type = EucalyptusCloudManager.message_type
         self.broker_service.message_store.set_accepted_types([message_type])
-        self.plugin = EucalyptusCloudManager()
-        self.manager.add(self.plugin)
+
+    def get_plugin(self, result=None):
+        plugin = EucalyptusCloudManager(
+            service_hub_factory=lambda data_path: FakeServiceHub(result),
+            eucalyptus_info_factory=lambda tools: FakeEucalyptusInfo(
+                fake_walrus_output, fake_cluster_controller_output,
+                fake_storage_controller_output, fake_node_controller_output))
+        self.manager.add(plugin)
+        return plugin
 
     def test_plugin_registers_with_a_name(self):
         """
         L{EucalyptusCloudManager} provides a C{plugin_name}, which is used
         when the plugin is registered with the manager plugin registry.
         """
-        plugin_name = self.plugin.plugin_name
-        self.assertIs(self.plugin, self.manager.get_plugin(plugin_name))
+        plugin = self.get_plugin()
+        self.assertIs(plugin, self.manager.get_plugin(plugin.plugin_name))
 
-    def test_wb_plugin_deregisters_on_imagestore_import_fail(self):
+    def test_run_with_successful_message(self):
+        """
+        If credentials are available and on problems occur while retrieving
+        information from C{euca_conf}, a message with information about
+        Eucalyptus is queued.
+        """
+        def check(ignore):
+            expected = {
+                "type": "eucalyptus-info",
+                "basic_info": {"access_key": "AcCeSsKeY123",
+                               "certificate_path": "/fake/path",
+                               "cloud_certificate_path": "/fake/path",
+                               "private_key_path": "/fake/path",
+                               "secret_key": None,
+                               "url_for_ec2": "http://fake/url",
+                               "url_for_s3": "http://fake/url"},
+                "cluster_controller_info": fake_cluster_controller_output,
+                "node_controller_info": fake_node_controller_output,
+                "storage_controller_info": fake_storage_controller_output,
+                "walrus_info": fake_walrus_output}
+            self.assertMessages(
+                self.broker_service.message_store.get_pending_messages(),
+                [expected])
+
+        plugin = self.get_plugin(succeed(FakeEucaInfo()))
+        deferred = plugin.run()
+        deferred.addCallback(check)
+        return deferred
+
+
+class EucalyptusCloudManagerWithoutImageStoreTest(LandscapeTest):
+
+    helpers = [ManagerHelper]
+
+    def setUp(self):
+        super(EucalyptusCloudManagerWithoutImageStoreTest, self).setUp()
+        message_type = EucalyptusCloudManager.message_type
+        self.broker_service.message_store.set_accepted_types([message_type])
+
+    def test_plugin_deregisters_on_imagestore_import_fail(self):
         """
         When L{EucalyptusCloudManager.run} is called it tries to import code
         from the C{imagestore} package.  The plugin disables itself if an
-        exception is raised during this process.
+        exception is raised during this process (such as C{ImportError}, for
+        example).
         """
-        plugin_name = self.plugin.plugin_name
-        self.assertIs(self.plugin, self.manager.get_plugin(plugin_name))
-        self.plugin._start_service_hub = lambda: 1/0
-        self.plugin.run()
-        self.assertRaises(KeyError, self.manager.get_plugin, plugin_name)
+        plugin = EucalyptusCloudManager(service_hub_factory=lambda: 1/0)
+        self.manager.add(plugin)
+        self.assertIs(plugin, self.manager.get_plugin(plugin.plugin_name))
+        plugin.run()
+        self.assertRaises(KeyError, self.manager.get_plugin,
+                          plugin.plugin_name)
