93012d05d28e485dd08cc733454325197786ca4e
Kapil Thangavelu <kapil@canonical.com>
2010-04-23 10:09:01 -0400

    additional tests for messages when network devices changes or doesn't change between plugin runs

--- landscape/monitor/tests/test_networkdevice.py
+++ landscape/monitor/tests/test_networkdevice.py
@@ -8,20 +8,38 @@ class NetworkDeviceTest(LandscapeTest):
 
     helpers = [MonitorHelper]
 
+    def setUp(self):
+        super(NetworkDeviceTest, self).setUp()
+        self.plugin = NetworkDevice()
+        self.monitor.add(self.plugin)
+        self.broker_service.message_store.set_accepted_types(
+            [self.plugin.message_type])
+
     def test_get_network_device(self):
         """A message is sent with device info"""
-        plugin = NetworkDevice()
-        self.monitor.add(plugin)
-        self.broker_service.message_store.set_accepted_types(
-            [plugin.message_type])
-        plugin.exchange()
+        self.plugin.exchange()
         message = self.mstore.get_pending_messages()[0]
         self.assertEquals(message["type"], "network-device")
         self.failUnlessIn("devices", message)
         self.assertTrue(len(message["devices"]))
+        # only network we can truly assert is localhost
+        self.assertTrue(message["devices"][0]["interface"], "lo")
+        self.assertTrue(message["devices"][0]["ip_address"], "0.0.0.0")
 
     def test_no_message_with_no_changes(self):
         """If no device changes from the last message, no message is sent."""
+        self.plugin.exchange()
+        self.mstore.delete_all_messages()
+        self.plugin.exchange()
+        self.assertFalse(self.mstore.count_pending_messages())
 
     def test_message_on_device_change(self):
         """When the active network devices change a message is generated."""
+        self.plugin.exchange()
+        self.mstore.delete_all_messages()
+        plugin = self.mocker.patch(self.plugin)
+        plugin._device_info()
+        self.mocker.result([])
+        self.mocker.replay()
+        self.plugin.exchange()
+        self.assertTrue(self.mstore.count_pending_messages())
