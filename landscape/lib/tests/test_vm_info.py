05e4d057dc8d098b1d123b602a3e01d8926c3810
Kevin McDermott <kevin@canonical.com>
2011-06-10 17:19:29 -0400

    Add new lib.vm_info code.

---
+++ landscape/lib/tests/test_vm_info.py
@@ -0,0 +1,122 @@
+import os
+
+from landscape.tests.helpers import LandscapeTest
+
+from landscape.lib.vm_info import get_vm_info
+from landscape.tests.mocker import ANY
+
+
+class VMInfoTest(LandscapeTest):
+
+    sample_kvm_cpuinfo = """
+processor	: 0
+vendor_id	: GenuineIntel
+cpu family	: 6
+model		: 2
+model name	: QEMU Virtual CPU version 0.14.0
+stepping	: 3
+cpu MHz		: 2653.112
+cache size	: 4096 KB
+fpu		: yes
+fpu_exception	: yes
+cpuid level	: 4
+wp		: yes
+flags		: fpu de pse tsc msr pae mce cx8 apic sep mtrr pge mca
+bogomips	: 5306.22
+clflush size	: 64
+cache_alignment	: 64
+address sizes	: 40 bits physical, 48 bits virtual
+power management:
+"""
+
+    def test_get_vm_info_empty_when_no_virtualization_is_found(self):
+        """
+        L{get_vm_info} should be empty when there's no virtualisation.
+        """
+        root_path = self.makeDir()
+        self.assertEqual(u"", get_vm_info(root_path=root_path))
+
+    def test_get_vm_info_is_openvz_when_proc_vz_exists(self):
+        """
+        L{get_vm_info} should return 'openvz' when /proc/vz exists.
+        """
+        root_path = self.makeDir()
+        proc_path = os.path.join(root_path, "proc")
+        self.makeDir(path=proc_path)
+
+        proc_vz_path = os.path.join(proc_path, "vz")
+        self.makeFile(path=proc_vz_path, content="foo")
+
+        self.assertEqual("openvz", get_vm_info(root_path=root_path))
+
+    def test_get_vm_info_is_xen_when_proc_sys_xen_exists(self):
+        """
+        L{get_vm_info} should return 'xen' when /proc/sys/xen exists.
+        """
+        root_path = self.makeDir()
+        proc_path = os.path.join(root_path, "proc")
+        self.makeDir(path=proc_path)
+
+        proc_sys_path = os.path.join(proc_path, "sys")
+        self.makeDir(path=proc_sys_path)
+
+        proc_sys_xen_path = os.path.join(proc_sys_path, "xen")
+        self.makeFile(path=proc_sys_xen_path, content="foo")
+
+        self.assertEqual("xen", get_vm_info(root_path=root_path))
+
+    def test_get_vm_info_is_xen_when_sys_bus_xen_exists(self):
+        """
+        L{get_vm_info} should return 'xen' when /sys/bus/xen exists.
+        """
+        root_path = self.makeDir()
+        sys_path = os.path.join(root_path, "sys")
+        self.makeDir(path=sys_path)
+
+        sys_bus_path = os.path.join(sys_path, "bus")
+        self.makeDir(path=sys_bus_path)
+
+        sys_bus_xen_path = os.path.join(sys_bus_path, "xen")
+        self.makeFile(path=sys_bus_xen_path, content="foo")
+
+        self.assertEqual("xen", get_vm_info(root_path=root_path))
+
+    def test_get_vm_info_is_xen_when_proc_xen_exists(self):
+        """
+        L{get_vm_info} should return 'xen' when /proc/xen exists.
+        """
+        root_path = self.makeDir()
+        proc_path = os.path.join(root_path, "proc")
+        self.makeDir(path=proc_path)
+
+        proc_xen_path = os.path.join(proc_path, "xen")
+        self.makeFile(path=proc_xen_path, content="foo")
+
+        self.assertEqual("xen", get_vm_info(root_path=root_path))
+
+    def test_get_vminfo_is_kvm_when_qemu_is_found_in_proc_cpuinfo(self):
+        """
+        L{get_vm_info} should return 'kvm' when QEMU Virtual CPU is found in
+        /proc/cpuinfo.
+        """
+        root_path = self.makeDir()
+        proc_path = os.path.join(root_path, "proc")
+        self.makeDir(path=proc_path)
+
+        cpuinfo_path = os.path.join(proc_path, "cpuinfo")
+        self.makeFile(path=cpuinfo_path, content=self.sample_kvm_cpuinfo)
+
+        self.assertEqual("kvm", get_vm_info(root_path=root_path))
+
+    def test_get_vm_info_is_empty_when_qemu_is_not_found_in_proc_cpuinfo(self):
+        """
+        L{get_vm_info} should have an empty string when QEMU Virtual CPU is not
+        found in /proc/cpuinfo.
+        """
+        root_path = self.makeDir()
+        proc_path = os.path.join(root_path, "proc")
+        self.makeDir(path=proc_path)
+
+        cpuinfo_path = os.path.join(proc_path, "cpuinfo")
+        self.makeFile(path=cpuinfo_path, content="foo")
+        self.assertEqual(u"", get_vm_info(root_path=root_path))
---
+++ landscape/lib/vm_info.py
@@ -0,0 +1,35 @@
+import os
+
+def get_vm_info(root_path):
+    """
+    This is a utility that returns the virtualization type
+
+    It loops through some possible configurations and return a string with
+    the name of the technology being used or None if there's no match
+    """
+    virt_info = ""
+
+    def join_root_path(path):
+        return os.path.join(root_path, path)
+
+    xen_paths = ["proc/sys/xen", "sys/bus/xen", "proc/xen"]
+    xen_paths = map(join_root_path, xen_paths)
+
+    vz_path = os.path.join(root_path, "proc/vz")
+    if os.path.exists(vz_path):
+        virt_info = "openvz"
+
+    elif filter(os.path.exists, xen_paths):
+        virt_info = "xen"
+
+    cpu_info_path = os.path.join(root_path, "proc/cpuinfo")
+    if os.path.exists(cpu_info_path):
+        try:
+            fd = open(cpu_info_path)
+            cpuinfo = fd.read()
+            if "QEMU Virtual CPU" in cpuinfo:
+                virt_info = "kvm"
+        finally:
+            fd.close()
+
+    return virt_info
