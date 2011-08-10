68e2c7f2e8981a47efda13a987aa1ae2c1a4fda3
Bjorn Tillenius <bjorn@canonical.com>
2011-08-10 16:04:35 +0300

    Send an empty apt-sources message.

--- landscape/message_schemas.py
+++ landscape/message_schemas.py
@@ -302,6 +302,9 @@ PACKAGE_REPORTER_RESULT = Message("package-reporter-result", {
     "code": Int(),
     "err": utf8})
 
+APT_SOURCES = Message("apt-sources", {
+    "sources": Dict(String(), Dict(String(), String()))})
+
 ADD_PACKAGES = Message("add-packages", {
     "packages": List(KeyDict({"name": utf8,
                               "description": Unicode(),
@@ -394,5 +397,5 @@ for schema in [ACTIVE_PROCESS_INFO, COMPUTER_UPTIME, CLIENT_UPTIME,
                ADD_PACKAGES, PACKAGE_REPORTER_RESULT, TEXT_MESSAGE, TEST,
                CUSTOM_GRAPH, REBOOT_REQUIRED, APT_PREFERENCES, EUCALYPTUS_INFO,
                EUCALYPTUS_INFO_ERROR, NETWORK_DEVICE, NETWORK_ACTIVITY,
-               REBOOT_REQUIRED_INFO]:
+               REBOOT_REQUIRED_INFO, APT_SOURCES]:
     message_schemas[schema.type] = schema
--- landscape/package/reporter.py
+++ landscape/package/reporter.py
@@ -58,6 +58,9 @@ class PackageReporter(PackageTaskHandler):
         # the SmartFacade will load freshly updated channels
         result.addCallback(lambda x: self.run_smart_update())
 
+        # Report which apt sources we're working with.
+        result.addCallback(lambda x: self.send_apt_sources())
+
         # If the appropriate hash=>id db is not there, fetch it
         result.addCallback(lambda x: self.fetch_hash_id_db())
 
@@ -215,6 +218,12 @@ class PackageReporter(PackageTaskHandler):
             "type": "package-reporter-result",
             "code": code,
             "err": err}
+        return self._broker.send_message(message, True)
+
+    def send_apt_sources(self):
+        message = {
+            "type": "apt-sources",
+            "sources": {}}
         return self._broker.send_message(message, True)
 
     def handle_task(self, task):
