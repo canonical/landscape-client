fe51c3994e1e011f90c7305684d419312737dca9
Geoff Teale <geoffrey.teale@canonical.com>
2011-12-23 15:46:54 +0100

    Allow view to locate, load and pack UI elements from a Glade file

--- landscape/ui/view/configuration.py
+++ landscape/ui/view/configuration.py
@@ -1,8 +1,29 @@
+import os
+
 from gi.repository import Gtk
 
 
 class LandscapeClientSettingsDialog(Gtk.Dialog):
+
+    GLADE_FILE = "landscape-client-settings.glade"
     
     def __init__(self, controller, *args, **kwargs):
         super(LandscapeClientSettingsDialog, self).__init__(*args, **kwargs)
         self.controller = controller
+        self.__ui_path = os.path.join(controller.data_path, "ui",
+                                      LandscapeClientSettingsDialog.GLADE_FILE)
+                                      
+        self.__builder = Gtk.Builder()
+        self.__builder.add_from_file(self.__ui_path)
+        self.__setup_ui()
+        
+    def __setup_ui(self):
+        content_area = self.get_content_area()
+        vbox = self.__builder.get_object(
+            "landscape-client-settings-dialog-vbox")
+        vbox.unparent()
+        content_area.pack_start(vbox, expand=True, fill=True, padding=0)
+        
+        
+        
+
--- landscape/ui/view/tests/test_configuration.py
+++ landscape/ui/view/tests/test_configuration.py
@@ -1,14 +1,47 @@
+import sys
+
+from gi.repository import Gtk
+
 from landscape.tests.helpers import LandscapeTest
+from landscape.configuration import LandscapeSetupConfiguration
+from landscape.ui.view.configuration import LandscapeClientSettingsDialog
+from landscape.ui.controller.configuration import ConfigController
 
 
 class ConfigurationViewTest(LandscapeTest):
 
     def setUp(self):
-        super(ConfigControllerTest, self).setUp()
-        
+        super(ConfigurationViewTest, self).setUp()
+        config = """
+[client]
+data_path = %s
+http_proxy = http://proxy.localdomain:3192
+tags = a_tag
+url = https://landscape.canonical.com/message-system
+account_name = foo
+registration_password = bar
+computer_title = baz
+https_proxy = https://proxy.localdomain:6192
+ping_url = http://landscape.canonical.com/ping
+""" % sys.path[0]
+        self.config_filename = self.makeFile(config)
+        class MyLandscapeSetupConfiguration(LandscapeSetupConfiguration):
+            default_config_filenames = [self.config_filename]
+        self.config = MyLandscapeSetupConfiguration(None)
 
     def test_init(self):
         """
         Test that we correctly initialise the L{ConfigurationView} correctly
         from the controller.
         """
+        controller = ConfigController(self.config)
+        dialog = LandscapeClientSettingsDialog(controller)
+        content_area = dialog.get_content_area()
+        children = content_area.get_children()
+        self.assertEqual(len(children), 2)
+        box = children[0]
+        self.assertIsInstance(box, Gtk.Box)
+        
+        
+        
+        
