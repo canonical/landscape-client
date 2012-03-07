008975f4552e55f1614a651c4e0a2d851cded9ab
Thomas Hervé <thomas@canonical.com>
2012-03-07 09:31:44 +0100

    Lint

--- landscape/upgraders/__init__.py
+++ landscape/upgraders/__init__.py
@@ -3,8 +3,7 @@ from landscape.upgraders import legacy, broker, monitor, package
 
 UPGRADE_MANAGERS = {
     # these should not be hardcoded
-    "legacy" : legacy.upgrade_manager,
-    "broker" : broker.upgrade_manager,
-    "monitor" : monitor.upgrade_manager,
-    "package" : package.upgrade_manager,
-    }
+    "legacy": legacy.upgrade_manager,
+    "broker": broker.upgrade_manager,
+    "monitor": monitor.upgrade_manager,
+    "package": package.upgrade_manager}
