55c4c276875597a801e1c8f3204fd151a468573d
Christopher Glass <christopher.glass@canonical.com>
2013-01-02 12:55:20 +0100

    Lint

--- landscape/lib/hashlib.py
+++ landscape/lib/hashlib.py
@@ -7,14 +7,14 @@ except ImportError:
     from md5 import md5
     from sha import sha as sha1
 else:
-    md5  = _hashlib.md5
+    md5 = _hashlib.md5
     sha1 = _hashlib.sha1
 
 
 __all__ = ["md5", "sha1"]
 
 
-def md5sum_file(filename, block_size=2**7):
+def md5sum_file(filename, block_size=128):
     """
     Return the MD5 hex digest of a  file by splitting it into chunks of
     C{block_size} so as to fit all the file in memory.
