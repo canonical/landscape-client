3f3aa1b7238f79f4b4a1ff993b5ab2b3edf181ce
Free Ekanayaka <free.ekanayaka@canonical.com>
2013-04-29 13:04:41 +0200

    Drop RemoteObjectConnector, add functional tests

--- landscape/lib/amp.py
+++ landscape/lib/amp.py
@@ -395,7 +395,7 @@ class RemoteObject(object):
         @param protocol: The newly connected protocol instance.
         """
         self._sender = MethodCallSender(protocol, self._factory.clock)
-        if self._retry_on_reconnect:
+        if self._factory.retryOnReconnect:
             self._retry()
 
     def _retry(self):
@@ -532,52 +532,3 @@ class MethodCallClientFactory(ReconnectingClientFactory):
 
         for deferred in requests:
             deferred.callback(result)
-
-
-class RemoteObjectConnector(object):
-    """Connect to remote objects exposed by a L{MethodCallProtocol}."""
-
-    factory = MethodCallClientFactory
-    remote = RemoteObject
-
-    def __init__(self, reactor, socket_path):
-        """
-        @param reactor: A reactor able to connect to Unix sockets.
-        @param socket: The path to the socket we want to connect to.
-        @param args: Arguments to be passed to the created L{RemoteObject}.
-        @param kwargs: Keyword arguments for the created L{RemoteObject}.
-        """
-        self._socket_path = socket_path
-        self._reactor = reactor
-        self._factory = None
-        self._connector = None
-
-    def connect(self, max_retries=None, factor=None):
-        """Connect to a remote object exposed by a L{MethodCallProtocol}.
-
-        This method will connect to the socket provided in the constructor
-        and return a L{Deferred} resulting in a connected L{RemoteObject}.
-
-        @param max_retries: If not C{None} give up try to connect after this
-            amount of times, otherwise keep trying to connect forever.
-        @param factor: Optionally a float indicating by which factor the
-            delay between subsequent retries should increase. Smaller values
-            result in a faster reconnection attempts pace.
-        """
-        factory = self.factory(self._reactor)
-        factory.maxRetries = max_retries
-        if factor:
-            factory.factor = factor
-        self._connector = self._reactor.connectUNIX(self._socket_path, factory)
-        return factory.getRemoteObject()
-
-    def disconnect(self):
-        """Disconnect the L{RemoteObject} that we have created."""
-        if self._connector is not None:
-            factory = self._connector.factory
-            factory.stopTrying()
-            # XXX we should be using self._connector.disconnect() here
-            remote = factory._remote
-            if remote:
-                if remote._sender.protocol.transport:
-                    remote._sender.protocol.transport.loseConnection()
