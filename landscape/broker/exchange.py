b6fbe4cddad8e555c41c145d1125870209d073da
tealeg <geoffrey.teale@canonical.com>
2013-05-21 14:48:12 +0200

    Add a comment explaining why we call drop_session_ids within the _resynchronize handler.

--- landscape/broker/exchange.py
+++ landscape/broker/exchange.py
@@ -332,6 +332,9 @@ class MessageExchange(object):
         self._reactor.fire("resynchronize-clients")
 
     def _resynchronize(self):
+        # When re-synchronisation occurs we don't want any previous messages
+        # being sent to the server, dropping the existing session_ids means
+        # that messages sent with those IDs will be dropped by the broker.
         self._message_store.drop_session_ids()
         self.schedule_exchange(urgent=True)
 
