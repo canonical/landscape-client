ef5153ca809dbcbe860451c37a6dd85c491262fc
Alberto Donato <alberto.donato@canonical.com>
2013-06-03 12:10:27 +0200

    Test cleanups.

--- landscape/monitor/tests/test_cephusage.py
+++ landscape/monitor/tests/test_cephusage.py
@@ -64,84 +64,73 @@ class CephUsagePluginTest(LandscapeTest):
     def setUp(self):
         super(CephUsagePluginTest, self).setUp()
         self.mstore = self.broker_service.message_store
+        self.plugin = CephUsage(create_time=self.reactor.time)
 
     def test_get_ceph_usage_if_command_not_found(self):
         """
         When the ceph command cannot be found or accessed, the
         C{_get_ceph_usage} method returns None.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-        plugin._get_status_command_output = lambda: None
-        self.monitor.add(plugin)
+        self.plugin._get_status_command_output = lambda: None
+        self.monitor.add(self.plugin)
 
-        result = plugin._get_ceph_usage()
-        self.assertIs(None, result)
+        self.assertIs(None, self.plugin._get_ceph_usage())
 
     def test_get_ceph_usage(self):
         """
         When the ceph command call returns output, the _get_ceph_usage method
         returns the percentage of used space.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-        plugin._get_status_command_output = lambda: SAMPLE_OUTPUT
-        self.monitor.add(plugin)
+        self.plugin._get_status_command_output = lambda: SAMPLE_OUTPUT
+        self.monitor.add(self.plugin)
 
-        result = plugin._get_ceph_usage()
-        self.assertEqual(0.12029780564263323, result)
+        self.assertEqual(0.12029780564263323, self.plugin._get_ceph_usage())
 
     def test_get_ceph_usage_old_format(self):
         """
         The _get_ceph_usage method understands command output in the "old"
         format (the output changed around version 0.56.1)
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-        plugin._get_status_command_output = lambda: SAMPLE_OLD_OUTPUT
-        self.monitor.add(plugin)
+        self.plugin._get_status_command_output = lambda: SAMPLE_OLD_OUTPUT
+        self.monitor.add(self.plugin)
 
-        result = plugin._get_ceph_usage()
-        self.assertEqual(0.12029780564263323, result)
+        self.assertEqual(0.12029780564263323, self.plugin._get_ceph_usage())
 
     def test_get_ceph_usage_empty_disk(self):
         """
         When the ceph command call returns output for empty disks, the
         _get_ceph_usage method returns 0.0 .
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-        plugin._get_status_command_output = (
+        self.plugin._get_status_command_output = (
             lambda: SAMPLE_NEW_TEMPLATE % (0, 100, 100))
-        self.monitor.add(plugin)
+        self.monitor.add(self.plugin)
 
-        result = plugin._get_ceph_usage()
-        self.assertEqual(0.0, result)
+        self.assertEqual(0.0, self.plugin._get_ceph_usage())
 
     def test_get_ceph_usage_full_disk(self):
         """
         When the ceph command call returns output for empty disks, the
         _get_ceph_usage method returns 1.0 .
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-        plugin._get_status_command_output = (
+        self.plugin._get_status_command_output = (
             lambda: SAMPLE_NEW_TEMPLATE % (100, 0, 100))
-        self.monitor.add(plugin)
 
-        result = plugin._get_ceph_usage()
-        self.assertEqual(1.0, result)
+        self.monitor.add(self.plugin)
+        self.assertEqual(1.0, self.plugin._get_ceph_usage())
 
     def test_get_ceph_usage_no_information(self):
         """
         When the ceph command outputs something that does not contain the
         disk usage information, the _get_ceph_usage method returns None.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
         output = "Blah\nblah"
         error = "Could not parse command output: '%s'" % output
         self.log_helper.ignore_errors(error)
 
-        plugin._get_status_command_output = lambda: output
-        self.monitor.add(plugin)
+        self.plugin._get_status_command_output = lambda: output
 
-        result = plugin._get_ceph_usage()
-        self.assertEqual(None, result)
+        self.monitor.add(self.plugin)
+        self.assertIs(None, self.plugin._get_ceph_usage())
 
     def test_never_exchange_empty_messages(self):
         """
@@ -150,12 +139,10 @@ class CephUsagePluginTest(LandscapeTest):
         message is created during exchange, it should not be queued.
         """
         self.mstore.set_accepted_types(["ceph-usage"])
-
-        plugin = CephUsage(create_time=self.reactor.time)
-        self.monitor.add(plugin)
+        self.monitor.add(self.plugin)
 
         self.monitor.exchange()
-        self.assertEqual(len(self.mstore.get_pending_messages()), 0)
+        self.assertEqual(0, len(self.mstore.get_pending_messages()))
 
     def test_exchange_messages(self):
         """
@@ -165,13 +152,11 @@ class CephUsagePluginTest(LandscapeTest):
         ring_id = "whatever"
         self.mstore.set_accepted_types(["ceph-usage"])
 
-        plugin = CephUsage(create_time=self.reactor.time)
-        plugin._ceph_usage_points = [(60, 1.0)]
-        plugin._ceph_ring_id = ring_id
-        self.monitor.add(plugin)
+        self.plugin._ceph_usage_points = [(60, 1.0)]
+        self.plugin._ceph_ring_id = ring_id
+        self.monitor.add(self.plugin)
 
         self.monitor.exchange()
-
         self.assertMessages(self.mstore.get_pending_messages(),
                             [{"type": "ceph-usage",
                               "ceph-usages": [(60, 1.0)],
@@ -181,13 +166,10 @@ class CephUsagePluginTest(LandscapeTest):
         """
         Calling create_message returns an expected message.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-        self.monitor.add(plugin)
-
         ring_id = "blah"
-        plugin._ceph_usage_points = []
-        plugin._ceph_ring_id = ring_id
-        message = plugin.create_message()
+        self.plugin._ceph_usage_points = []
+        self.plugin._ceph_ring_id = ring_id
+        message = self.plugin.create_message()
 
         self.assertIn("type", message)
         self.assertEqual(message["type"], "ceph-usage")
@@ -197,8 +179,8 @@ class CephUsagePluginTest(LandscapeTest):
         self.assertEqual(len(ceph_usages), 0)
 
         point = (60, 1.0)
-        plugin._ceph_usage_points = [point]
-        message = plugin.create_message()
+        self.plugin._ceph_usage_points = [point]
+        message = self.plugin.create_message()
         self.assertIn("type", message)
         self.assertEqual(message["type"], "ceph-usage")
         self.assertIn("ceph-usages", message)
@@ -209,8 +191,8 @@ class CephUsagePluginTest(LandscapeTest):
 
     def test_no_message_if_not_accepted(self):
         """
-        Don't add any messages at all if the broker isn't currently
-        accepting their type.
+        Don't add any messages at all if the broker isn't currently accepting
+        their type.
         """
         interval = 30
         monitor_interval = 300
@@ -232,15 +214,9 @@ class CephUsagePluginTest(LandscapeTest):
         When given a well formatted command output, the _get_ceph_ring_id()
         method returns the correct ring_id.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-
         uuid = "i-am-a-uuid"
-
-        plugin._get_quorum_command_output = lambda: SAMPLE_QUORUM % uuid
-        self.monitor.add(plugin)
-
-        result = plugin._get_ceph_ring_id()
-        self.assertEqual(uuid, result)
+        self.plugin._get_quorum_command_output = lambda: SAMPLE_QUORUM % uuid
+        self.assertEqual(uuid, self.plugin._get_ceph_ring_id())
 
     def test_get_ceph_ring_id_valid_json_no_information(self):
         """
@@ -248,7 +224,6 @@ class CephUsagePluginTest(LandscapeTest):
         the ring uuid information present but that is valid JSON, the
         _get_ceph_ring_id method returns None.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
         error = "Could not get ring_id from output: '{\"election_epoch\": 8}'."
         self.log_helper.ignore_errors(error)
 
@@ -257,12 +232,9 @@ class CephUsagePluginTest(LandscapeTest):
             data = {"election_epoch": 8}
             return json.dumps(data)
 
-        plugin._get_quorum_command_output = return_output
+        self.plugin._get_quorum_command_output = return_output
 
-        self.monitor.add(plugin)
-
-        result = plugin._get_ceph_ring_id()
-        self.assertEqual(None, result)
+        self.assertIs(None, self.plugin._get_ceph_ring_id())
 
     def test_get_ceph_ring_id_no_information(self):
         """
@@ -270,15 +242,11 @@ class CephUsagePluginTest(LandscapeTest):
         the ring uuid information present, the _get_ceph_ring_id method returns
         None.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
         error = "Could not get ring_id from output: 'Blah\nblah'."
         self.log_helper.ignore_errors(error)
 
-        plugin._get_quorum_command_output = lambda: "Blah\nblah"
-        self.monitor.add(plugin)
-
-        result = plugin._get_ceph_ring_id()
-        self.assertEqual(None, result)
+        self.plugin._get_quorum_command_output = lambda: "Blah\nblah"
+        self.assertIs(None, self.plugin._get_ceph_ring_id())
 
     def test_get_ceph_ring_id_command_exception(self):
         """
@@ -286,12 +254,8 @@ class CephUsagePluginTest(LandscapeTest):
         exception happened for example), the _get_ceph_ring_id method
         returns None and logs no error.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-        plugin._get_quorum_command_output = lambda: None
-        self.monitor.add(plugin)
-
-        result = plugin._get_ceph_ring_id()
-        self.assertEqual(None, result)
+        self.plugin._get_quorum_command_output = lambda: None
+        self.assertIs(None, self.plugin._get_ceph_ring_id())
 
     def test_plugin_run(self):
         """
@@ -330,15 +294,13 @@ class CephUsagePluginTest(LandscapeTest):
         If the reactor fires a "resynchronize" even the C{_resynchronize}
         method on the ceph plugin object is called.
         """
-        plugin = CephUsage(create_time=self.reactor.time)
-
         self.called = False
 
         def stub_resynchronize():
             self.called = True
-        plugin._resynchronize = stub_resynchronize
+        self.plugin._resynchronize = stub_resynchronize
 
-        self.monitor.add(plugin)
+        self.monitor.add(self.plugin)
 
         self.reactor.fire("resynchronize")
 
