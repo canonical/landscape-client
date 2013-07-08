68170dd008cf9fade976d4a173f5b19cc65a98e2
Geoffrey Teale <geoffrey.teale@canonical.com>
2013-07-08 18:18:45 +0200

    Make scopes optional in resynchronize messages.

--- landscape/message_schemas.py
+++ landscape/message_schemas.py
@@ -141,7 +141,7 @@ RESYNCHRONIZE = Message(
      "scopes": List(String())},
     # operation-id is only there if it's a response to a server-initiated
     # resynchronize.
-    optional=["operation-id"])
+    optional=["operation-id", "scopes"])
 
 MOUNT_ACTIVITY = Message("mount-activity", {
     "activities": List(Tuple(Float(), utf8, Bool()))})
