f4c557745aba9fe72075934762d44e3433895ae1
Christopher Glass <christopher.glass@canonical.com>
2014-06-09 10:18:05 +0200

    Reverted merge from trunk that apparently destroyed most changes the previous branch introduced.

--- landscape/lib/juju.py
+++ landscape/lib/juju.py
@@ -1,26 +1,36 @@
 import json
 import logging
-import os.path
+
+from glob import glob
 
 from landscape.lib.fs import read_file
 
 
 def get_juju_info(config):
     """
-    Returns the Juju info or C{None} if the path referenced from
-    L{config} is not a file or that file isn't valid JSON.
+    Returns the list of Juju info or C{None} if the path referenced from
+    L{config} is not a valid directory.
+
+    The list of juju info is constructed by appending all the contents of
+    *.json files found in the path referenced from the L{config}.
     """
-    juju_filename = config.juju_filename
-    if not os.path.isfile(juju_filename):
-        return None
-    json_contents = read_file(juju_filename)
-    try:
-        juju_info = json.loads(json_contents)
-    except Exception:
-        logging.exception(
-            "Error attempting to read JSON from %s" % juju_filename)
-        return None
-    else:
-        if "api-addresses" in juju_info:
-            juju_info["api-addresses"] = juju_info["api-addresses"].split()
-        return juju_info
+    juju_directory = config.juju_directory
+
+    juju_info_list = []
+
+    for juju_file in glob("%s/*.json" % juju_directory):
+
+        json_contents = read_file(juju_file)
+        try:
+            juju_info = json.loads(json_contents)
+        except Exception:
+            logging.exception(
+                "Error attempting to read JSON from %s" % juju_file)
+            return None
+        else:
+            if "api-addresses" in juju_info:
+                split = juju_info["api-addresses"].split()
+                juju_info["api-addresses"] = split
+            juju_info_list.append(juju_info)
+
+    return juju_info_list or None
