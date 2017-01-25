6d3a5d1cfcb3be58a9b128a7d26d9b88adb58665
Ursula Junque (Ursinha) <ursula.junque@canonical.com>
2017-01-25 17:02:03 -0200

    Moving bzr-builddeb conf file from deprecated location to debian/, this fixes lp #1658796 and allows using bzr builddeb

--- landscape/__init__.py
+++ landscape/__init__.py
@@ -1,5 +1,5 @@
 DEBIAN_REVISION = ""
-UPSTREAM_VERSION = "17.01"
+UPSTREAM_VERSION = "17.01+bzr932"
 VERSION = "%s%s" % (UPSTREAM_VERSION, DEBIAN_REVISION)
 
 # The minimum server API version that all Landscape servers are known to speak
