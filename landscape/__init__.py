8dcac42c3e47d6b1603800b27ed4940e3a41aff1
Alberto Donato <alberto.donato@canonical.com>
2017-03-02 10:36:25 +0100

    Revert accidental version change, bump version to 17.02 [trivial] [r=bjornt].

--- landscape/__init__.py
+++ landscape/__init__.py
@@ -1,5 +1,5 @@
-DEBIAN_REVISION = "-0ubuntu0"
-UPSTREAM_VERSION = "17.01+bzr946"
+DEBIAN_REVISION = ""
+UPSTREAM_VERSION = "17.02"
 VERSION = "%s%s" % (UPSTREAM_VERSION, DEBIAN_REVISION)
 
 # The minimum server API version that all Landscape servers are known to speak
