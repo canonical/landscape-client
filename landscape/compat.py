4fbc7a5c17ab7a43a58a6034836479e1a60dc56e
Steffen Allner <sa@gocept.com>
2017-04-04 16:34:13 +0200

    We do not seem to need coerce_unicode() anymore, so remove it.

--- landscape/compat.py
+++ landscape/compat.py
@@ -22,29 +22,3 @@ else:
     stringio = StringIO
     from cStringIO import StringIO as cstringio
     input = raw_input
-
-
-def coerce_unicode(s, encoding='ascii', errors='strict'):
-    """
-    Coerce byte strings into unicode for Python 2.
-
-    In Python 2, decodes a byte string L{s} into unicode using the L{encoding},
-    returns unmodified if any other type. In Python 3, raises a L{TypeError}
-    when passed a byte string in L{s}, returns unmodified otherwise.
-
-    @param s: The string to be converted to unicode.
-    @type s: L{bytes} or L{unicode}
-    @raise UnicodeError: The input L{bytes} is not decodable
-        with given encoding.
-    @raise TypeError: The input is L{bytes} on Python 3.
-    """
-    # In Python 2 C{unicode(b'bytes')} returns a unicode string C{'bytes'}. In
-    # Python 3, the equivalent C{str(b'bytes')} will return C{"b'bytes'"}
-    # instead.
-    if isinstance(s, bytes):
-        if _PY3:
-            raise TypeError("Expected str not %r (bytes)" % (s,))
-        else:
-            return s.decode(encoding, errors)
-    else:
-        return s
