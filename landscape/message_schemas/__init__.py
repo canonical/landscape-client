19bc8d2b91ceb043b82048eaa01f5ce659444c64
Eric Snow <eric.snow@canonical.com>
2017-04-06 18:27:36 +0000

    Import from l.message_schemas.server_bound directly.

--- landscape/message_schemas/__init__.py
+++ landscape/message_schemas/__init__.py
@@ -1,5 +1,4 @@
 # Copyright 2017 Canonical Limited.  All rights reserved.
 
-# These imports are here for backward-compatibility.
+# This import is here for backward-compatibility.
 from .server_bound import *
-from .server_bound import message_schemas
