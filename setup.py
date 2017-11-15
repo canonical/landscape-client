702835c17e94770a835f2a5e6d53e802b0a72593
Eric Snow <ericsnowcurrently@gmail.com>
2017-11-15 19:42:39 +0000

    Split setup.py along the 3 divisions.

--- setup.py
+++ setup.py
@@ -1,47 +1,65 @@
 #!/usr/bin/python
 
 from distutils.core import setup
-import sys
 
-from landscape import UPSTREAM_VERSION
-
+# from python3-distutils-extra
 from DistUtilsExtra.command import build_extra
 from DistUtilsExtra.auto import clean_build_tree
 
+from landscape import UPSTREAM_VERSION
 
+
+SETUP = dict(
+        name=None,
+        description=None,
+        packages=None,
+        py_modules=None,
+        scripts=None,
+
+        version=UPSTREAM_VERSION,
+        author="Landscape Team",
+        author_email="landscape-team@canonical.com",
+        url="http://landscape.canonical.com",
+        cmdclass={"build": build_extra.build_extra,
+                  "clean": clean_build_tree},
+        )
+
+
+def setup_landscape(name, description, packages, modules=None, scripts=None,
+                    **kwargs):
+    assert name and description and packages
+    kwargs = dict(SETUP,
+                  name=name,
+                  description=description,
+                  packages=packages,
+                  py_modules=modules,
+                  scripts=scripts,
+                  **kwargs)
+    kwargs = {k: v for k, v in kwargs.items() if k is not None}
+    setup(**kwargs)
+
+
+# Import these afterward to avoid circular imports.
+import setup_lib, setup_sysinfo, setup_client
+
+PACKAGES = []
+MODULES = []
 SCRIPTS = []
-if sys.version_info[0] == 3:
-    SCRIPTS = [
-            "scripts/landscape-client",
-            "scripts/landscape-config",
-            "scripts/landscape-broker",
-            "scripts/landscape-manager",
-            "scripts/landscape-monitor",
-            "scripts/landscape-package-changer",
-            "scripts/landscape-package-reporter",
-            "scripts/landscape-release-upgrader",
-            "scripts/landscape-sysinfo",
-            ]
+DEB_REQUIRES = []
+REQUIRES = []
+for sub in (setup_lib, setup_sysinfo, setup_client):
+    PACKAGES += sub.PACKAGES
+    MODULES += sub.MODULES
+    SCRIPTS += sub.SCRIPTS
+    DEB_REQUIRES += sub.DEB_REQUIRES
+    REQUIRES += sub.REQUIRES
+    
 
-
-setup(name="Landscape Client",
-      version=UPSTREAM_VERSION,
-      description="Landscape Client",
-      author="Landscape Team",
-      author_email="landscape-team@canonical.com",
-      url="http://landscape.canonical.com",
-      packages=["landscape",
-                "landscape.client",
-                "landscape.client.broker",
-                "landscape.client.manager",
-                "landscape.client.monitor",
-                "landscape.client.package",
-                "landscape.client.upgraders",
-                "landscape.client.user",
-                "landscape.lib",
-                "landscape.message_schemas",
-                "landscape.sysinfo",
-                ],
-      scripts=SCRIPTS,
-      cmdclass={"build": build_extra.build_extra,
-                "clean": clean_build_tree})
+if __name__ == "__main__":
+    setup_landscape(
+        name="landscape-client",
+        description="Landscape Client",
+        packages=PACKAGES,
+        modules=MODULES,
+        scripts=SCRIPTS,
+        )
