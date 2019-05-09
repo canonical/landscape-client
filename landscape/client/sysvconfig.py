dfc3086656d3ce1ec1e1bf1a77ca32c4c8197f70
Simon Poirier <simon.poirier@canonical.com>
2019-05-09 16:14:17 -0400

    config and service descriptors

--- landscape/client/sysvconfig.py
+++ landscape/client/sysvconfig.py
@@ -1,60 +1,42 @@
 """Programmatically manage the Landscape client SysV-style init script."""
-
-import os
+from subprocess import Popen
 
 
 class ProcessError(Exception):
     """ Error running a process with os.system. """
 
 
-class SysVConfig(object):
-    """Configure and drive the Landscape client init script.
+class SystemdConfig(object):
+    """Configure and drive the Landscape client service.
 
-    @param filename: Path to the file holding init scripts env variables.
+    @param filename: Path to the file holding service env variables.
     """
 
     def __init__(self, filename="/etc/default/landscape-client"):
         self._filename = filename
 
     def set_start_on_boot(self, flag):
-        """Make the init script decide to start the client when it's run."""
-        current = self._parse_file()
-        current["RUN"] = flag and 1 or 0
-        self._write_file(current)
+        """Make the service decide to start the client when it's run."""
+        action = "enable" if flag else "disable"
+        Popen(["systemctl", action, "landscape-client.service"]).wait()
 
     def restart_landscape(self):
         """Restart the Landscape client service."""
-        if os.system("/etc/init.d/landscape-client restart"):
+        if Popen(["systemctl", "restart", "landscape-client.service"]).wait():
             raise ProcessError("Could not restart client")
 
     def stop_landscape(self):
         """Stop the Landscape client service."""
-        if os.system("/etc/init.d/landscape-client stop"):
+        if Popen(["systemctl", "stop", "landscape-client.service"]).wait():
             raise ProcessError("Could not stop client")
 
     def is_configured_to_run(self):
         """
-        Return a boolean representing whether the init script will decide to
-        actually start the client when it is run.  This method should match
-        the semantics of the checks in debian/landscape-client.init.
+        Return a boolean representing whether the service will start on boot.
         """
-        state = self._parse_file()
-        run_value = state.get("RUN", "0")
-        return (not run_value[:1].isspace()) and run_value != "0"
+        cmd = ["systemctl", "is-enabled", "landscape-client.service"]
+        return Popen(cmd).wait() == 0
 
-    def _parse_file(self):
-        values = {}
-        # Only attempt to parse the file if it exists.
-        if os.path.isfile(self._filename):
-            for line in open(self._filename, "r"):
-                line = line.strip()
-                if "=" in line:
-                    key, value = line.split("=")
-                    values[key] = value
-        return values
 
-    def _write_file(self, values):
-        file = open(self._filename, "w")
-        for key in sorted(values.keys()):
-            file.write("%s=%s\n" % (key, str(values[key])))
-        file.close()
+# Deprecated alias for charms and scripts still using the old name.
+SysVConfig = SystemdConfig
