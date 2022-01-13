8d19a860db2690af054bfc278017374de8df6f1d
Kevin Nasto <kevinnasto@gmail.com>
2022-01-13 12:09:20 -0600

    Refactored to default args in __init__ to avoid mixing class and instance attributes, Added docstring

--- landscape/client/monitor/computertags.py
+++ landscape/client/monitor/computertags.py
@@ -7,17 +7,21 @@ from landscape.lib.tag import is_valid_tag_list
 
 
 class ComputerTags(DataWatcher):
+    """Plugin watches config file for changes in computer tags"""
 
     persist_name = "computer-tags"
     message_type = "computer-tags"
     message_key = "tags"
     run_interval = 3600  # Every hour only when data changed
     run_immediately = True
-    _argv = sys.argv  # To mock argv in unit tests
+
+    def __init__(self, args=sys.argv):
+        super(ComputerTags, self).__init__()
+        self.args = sys.argv  # Defined to specify args in unit tests
 
     def get_data(self):
         config = BrokerConfiguration()
-        config.load(ComputerTags._argv)  # Load the default or specified config
+        config.load(self.args)  # Load the default or specified config
         tags = config.tags
         if not is_valid_tag_list(tags):
             tags = None
