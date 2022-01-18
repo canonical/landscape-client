62b470757c4d27745372fd1291474135108c00cf
Kevin Nasto <kevinnasto@gmail.com>
2022-01-18 14:20:44 -0600

    Computer tags message plugin (#100)
    * Initial commit for tags message
    
    * Added tests
    
    * Added plugin name for tags
    
    * Tags changes to use sys.argv and run immediately
    
    * Added test case to read tags from command line args

---
+++ landscape/client/monitor/computertags.py
@@ -0,0 +1,29 @@
+import logging
+import sys
+
+from landscape.client.broker.config import BrokerConfiguration
+from landscape.client.monitor.plugin import DataWatcher
+from landscape.lib.tag import is_valid_tag_list
+
+
+class ComputerTags(DataWatcher):
+    """Plugin watches config file for changes in computer tags"""
+
+    persist_name = "computer-tags"
+    message_type = "computer-tags"
+    message_key = "tags"
+    run_interval = 3600  # Every hour only when data changed
+    run_immediately = True
+
+    def __init__(self, args=sys.argv):
+        super(ComputerTags, self).__init__()
+        self.args = args  # Defined to specify args in unit tests
+
+    def get_data(self):
+        config = BrokerConfiguration()
+        config.load(self.args)  # Load the default or specified config
+        tags = config.tags
+        if not is_valid_tag_list(tags):
+            tags = None
+            logging.warning("Invalid tags provided for computer-tags message.")
+        return tags
---
+++ landscape/client/monitor/tests/test_computertags.py
@@ -0,0 +1,64 @@
+from landscape.client.monitor.computertags import ComputerTags
+from landscape.client.tests.helpers import MonitorHelper, LandscapeTest
+
+
+class ComputerTagsTest(LandscapeTest):
+
+    helpers = [MonitorHelper]
+
+    def setUp(self):
+        super(ComputerTagsTest, self).setUp()
+        test_sys_args = ['hello.py']
+        self.plugin = ComputerTags(args=test_sys_args)
+        self.monitor.add(self.plugin)
+
+    def test_tags_are_read(self):
+        """
+        Tags are read from the default config path
+        """
+        tags = 'check,linode,profile-test'
+        file_text = "[client]\ntags = {}".format(tags)
+        config_filename = self.config.default_config_filenames[0]
+        self.makeFile(file_text, path=config_filename)
+        self.assertEqual(self.plugin.get_data(), tags)
+
+    def test_tags_are_read_from_args_path(self):
+        """
+        Tags are read from path specified in command line args
+        """
+        tags = 'check,linode,profile-test'
+        file_text = "[client]\ntags = {}".format(tags)
+        filename = self.makeFile(file_text)
+        test_sys_args = ['hello.py', '--config', filename]
+        self.plugin.args = test_sys_args
+        self.assertEqual(self.plugin.get_data(), tags)
+
+    def test_tags_message_sent(self):
+        """
+        Tags message is sent correctly
+        """
+        tags = 'check,linode,profile-test'
+        file_text = "[client]\ntags = {}".format(tags)
+        config_filename = self.config.default_config_filenames[0]
+        self.makeFile(file_text, path=config_filename)
+
+        self.mstore.set_accepted_types(["computer-tags"])
+        self.plugin.exchange()
+        messages = self.mstore.get_pending_messages()
+        self.assertEqual(messages[0]['tags'], tags)
+
+    def test_invalid_tags(self):
+        """
+        If invalid tag detected then message contents should be None
+        """
+        tags = 'check,lin ode'
+        file_text = "[client]\ntags = {}".format(tags)
+        config_filename = self.config.default_config_filenames[0]
+        self.makeFile(file_text, path=config_filename)
+        self.assertEqual(self.plugin.get_data(), None)
+
+    def test_empty_tags(self):
+        """
+        Makes sure no errors when tags section is empty
+        """
+        self.assertEqual(self.plugin.get_data(), None)
