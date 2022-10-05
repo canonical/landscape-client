5ed7a4541256fa65f435f9a789c6687fe4320d29
Mitch Burton <mitch.burton@canonical.com>
2022-10-05 12:38:49 -0700

    allow lsb_release file reading optionally and as fallback; fix tests

--- landscape/lib/lsb_release.py
+++ landscape/lib/lsb_release.py
@@ -1,19 +1,58 @@
 """Get information from /usr/bin/lsb_release."""
+import os
+from subprocess import DEVNULL, CalledProcessError, check_output
 
-LSB_RELEASE_FILENAME = "/usr/bin/lsb_release"
-LSB_RELEASE_INFO_KEYS = {"distributor-id": "-si",
-                         "release": "-sr",
-                         "code-name": "-sc",
-                         "description": "-sd"}
+LSB_RELEASE = "/usr/bin/lsb_release"
+LSB_RELEASE_FILENAME = "/etc/lsb_release"
+LSB_RELEASE_FILE_KEYS = {
+    "DISTRIB_ID": "distributor-id",
+    "DISTRIB_DESCRIPTION": "description",
+    "DISTRIB_RELEASE": "release",
+    "DISTRIB_CODENAME": "code-name",
+}
 
-import subprocess
 
-def parse_lsb_release(lsb_release_filename):
-    """Return a C{dict} holding information about the system LSB release.
-    @raises: An OSError exception if C{lsb_release_filename} does not exist.
+def parse_lsb_release(lsb_release_filename=None):
+    """
+    Returns a C{dict} holding information about the system LSB release.
+    Reads from C{lsb_release_filename} if it exists, else calls
+    C{LSB_RELEASE}
+    """
+    if lsb_release_filename and os.path.exists(lsb_release_filename):
+        return parse_lsb_release_file(lsb_release_filename)
+
+    try:
+        lsb_info = check_output([LSB_RELEASE, "-as"], stderr=DEVNULL)
+    except CalledProcessError:
+        # Fall back to reading file, even if it doesn't exist.
+        return parse_lsb_release_file(lsb_release_filename)
+    else:
+        dist_id, desc, release, code_name, _ = lsb_info.decode().split("\n")
+
+        return {
+            "distributor-id": dist_id,
+            "release": release,
+            "code-name": code_name,
+            "description": desc,
+        }
+
+
+def parse_lsb_release_file(filename):
+    """
+    Returns a C{dict} holding information about the system LSB release
+    by attempting to parse C{filename}.
+
+    @raises: A FileNotFoundError if C{filename} does not exist.
     """
     info = {}
-    for key in LSB_RELEASE_INFO_KEYS:
-        value = subprocess.check_output([LSB_RELEASE_FILENAME] + [LSB_RELEASE_INFO_KEYS[key.strip()]])
-        info[key.strip()] = value.strip().strip('"')
+
+    with open(filename) as fd:
+        for line in fd:
+            key, value = line.split("=")
+
+            if key in LSB_RELEASE_FILE_KEYS:
+                key = LSB_RELEASE_FILE_KEYS[key.strip()]
+                value = value.strip().strip('"')
+                info[key] = value
+
     return info
