72f6168da819514b3893dac5e14fd97833264478
Bill Kronholm <bill.kronholm@canonical.com>
2023-10-11 23:21:02 +0000

    Release 23.10

--- landscape/__init__.py
+++ landscape/__init__.py
@@ -1,5 +1,5 @@
 DEBIAN_REVISION = ""
-UPSTREAM_VERSION = "23.08"
+UPSTREAM_VERSION = "23.10"
 VERSION = f"{UPSTREAM_VERSION}{DEBIAN_REVISION}"
 
 # The minimum server API version that all Landscape servers are known to speak
