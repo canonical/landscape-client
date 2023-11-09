0571c0289e540e6e43d43c4ffb77ab0cd688780b
mcw-work <111986660+mcw-work@users.noreply.github.com>
2023-11-09 12:04:31 +0000

    Update shutdownmanager.py
    Fix lint errors

--- landscape/client/manager/shutdownmanager.py
+++ landscape/client/manager/shutdownmanager.py
@@ -7,6 +7,7 @@ from landscape.client.manager.plugin import FAILED
 from landscape.client.manager.plugin import ManagerPlugin
 from landscape.client.manager.plugin import SUCCEEDED
 
+
 class ShutdownManager(ManagerPlugin):
     """
     Plugin that either shuts down or reboots the device.
@@ -14,13 +15,13 @@ class ShutdownManager(ManagerPlugin):
     In both cases, the manager sends the success command before attempting
     the shutdown/reboot.
     
-    With reboot - the call is instanteous but the success message will be 
+    With reboot - the call is instanteous but the success message will be
     send as soon as the device comes back up.
-    
+
     For shutdown there is a 120 second delay between sending the success and
     firing the shutdown. This is usually sufficent.
     """
-    
+
     def register(self, registry):
         super().register(registry)
         self.config = registry.config
@@ -51,7 +52,7 @@ class ShutdownManager(ManagerPlugin):
         logging.info("Sending Reboot Command")
         bus = dbus.SystemBus()
         bus_object = bus.get_object(
-            "org.freedesktop.login1", 
+            "org.freedesktop.login1",
             "/org/freedesktop/login1")
         bus_object.Reboot(True, dbus_interface="org.freedesktop.login1.Manager")
         
@@ -62,19 +63,19 @@ class ShutdownManager(ManagerPlugin):
             "org.freedesktop.login1",
             "/org/freedesktop/login1")
         bus_object.PowerOff(True, dbus_interface="org.freedesktop.login1.Manager")
-
+        
     def _respond_reboot_success(self, data, operation_id):
         deferred = self._respond(SUCCEEDED, data, operation_id)
         deferred.addCallback(self._Reboot)
         deferred.addErrback(self._respond_fail)
         return deferred
-
+        
     def _respond_shutdown_success(self, data, operation_id):
         deferred = self._respond(SUCCEEDED, data, operation_id)
         reactor.callLater(120, self._Shutdown)
         deferred.addErrback(self._respond_fail)
         return deferred
-
+        
     def _respond_fail(self, data, operation_id):
         logging.info("Shutdown/Reboot request failed.")
         deferred = self._respond(FAILED, data, operation_id)
