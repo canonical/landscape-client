c249735c2057ff75c542e0486c8f96388ea67611
Stephen Mwangi <mail@stephenmwangi.com>
2024-02-24 02:27:10 +0300

    fix: user management on classic snap (#218)

--- landscape/client/__init__.py
+++ landscape/client/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 IS_SNAP = os.getenv("LANDSCAPE_CLIENT_SNAP")
-IS_CORE = os.getenv("LANDSCAPE_CLIENT_CORE")
+IS_CORE = os.getenv("SNAP_SAVE_DATA") is not None
 
 USER = "root" if IS_SNAP else "landscape"
 GROUP = "root" if IS_SNAP else "landscape"
