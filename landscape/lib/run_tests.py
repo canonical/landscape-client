b8f5370d799a3dfb286a653b24fd9bf1b73c29fd
Mitch Burton <mitch.burton@canonical.com>
2024-07-29 13:50:35 -0700

    lint

--- landscape/lib/run_tests.py
+++ landscape/lib/run_tests.py
@@ -1,6 +1,7 @@
 if __name__ == "__main__":
-    # Patch Twisted Trial's `_AdaptedReporter` to add the Python 3.12-expected `addDuration` method.
-    # This eliminates a large amount of emitted warnings, and should no longer be necessary once
+    # Patch Twisted Trial's `_AdaptedReporter` to add the Python 3.12-expected
+    # `addDuration` method. This eliminates a large amount of emitted warnings,
+    # and should no longer be necessary once
     # https://github.com/twisted/twisted/issues/12229 is fixed.
     import sys
     from twisted.trial.reporter import _AdaptedReporter
