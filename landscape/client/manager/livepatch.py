0a66e0a98751f98bb5c66eedaf86bd3b88e3672e
HJK-X <justin.kim@canonical.com>
2024-09-05 13:12:25 -0700

    parse command output if format type is yaml

--- landscape/client/manager/livepatch.py
+++ landscape/client/manager/livepatch.py
@@ -2,8 +2,6 @@ import json
 import logging
 import subprocess
 import traceback
-import yaml
-
 
 from landscape.client.manager.plugin import DataWatcherManager
 
@@ -22,10 +20,22 @@ class LivePatch(DataWatcherManager):
     run_interval = 1800  # Every 30 min
 
     def get_data(self):
-        json_output = get_livepatch_status('json')
-        readable_output = get_livepatch_status('humane')
-        return json.dumps({'humane': readable_output, 'json': json_output},
-                          sort_keys=True)  # Prevent randomness for cache
+        json_output = get_livepatch_status("json")
+        readable_output = get_livepatch_status("humane")
+        return json.dumps(
+            {"humane": readable_output, "json": json_output}, sort_keys=True
+        )  # Prevent randomness for cache
+
+
+def _parse_humane(cmd_output):
+    entries = cmd_output.split("\n")
+    data = {}
+
+    for e in entries:
+        key, value = e.split(":", 1)
+        data[key] = value.strip()
+
+    return data
 
 
 def get_livepatch_status(format_type):
@@ -40,36 +50,47 @@ def get_livepatch_status(format_type):
     try:
         completed_process = subprocess.run(
             ["canonical-livepatch", "status", "--format", format_type],
-            encoding="utf8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            encoding="utf8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
     except FileNotFoundError as exc:
-        data['return_code'] = -1
-        data['error'] = str(exc)
-        data['output'] = ""
+        data["return_code"] = -1
+        data["error"] = str(exc)
+        data["output"] = ""
     except Exception as exc:
-        data['return_code'] = -2
-        data['error'] = str(exc)
-        data['output'] = ""
+        data["return_code"] = -2
+        data["error"] = str(exc)
+        data["output"] = ""
         logging.error(traceback.format_exc())
     else:
         output = completed_process.stdout.strip()
         try:
             if output:  # We don't want to parse an empty string
-                if format_type == 'json':
+                if format_type == "json":
                     output = json.loads(output)
-                    if 'Last-Check' in output:  # Remove timestamps for cache
-                        del output['Last-Check']
-                    if 'Uptime' in output:
-                        del output['Uptime']
+                    if "Last-Check" in output:  # Remove timestamps for cache
+                        del output["Last-Check"]
+                    if "Uptime" in output:
+                        del output["Uptime"]
                 else:
-                    output = yaml.safe_load(output)
-                    if 'last check' in output:
-                        del output['last check']
-            data['return_code'] = completed_process.returncode
-            data['error'] = completed_process.stderr
-            data['output'] = output
-        except (yaml.YAMLError, json.decoder.JSONDecodeError) as exc:
-            data['return_code'] = completed_process.returncode
-            data['error'] = str(exc)
-            data['output'] = output
+                    output = _parse_humane(output)
+                    if "last check" in output:
+                        del output["last check"]
+            data["return_code"] = completed_process.returncode
+            data["error"] = err or completed_process.stderr
+            data["output"] = output
+        except json.decoder.JSONDecodeError as exc:
+            data["return_code"] = completed_process.returncode
+            data["error"] = str(exc)
+            data["output"] = output
+
+            # "last check: 16 seconds ago\n
+            # kernel: 6.8.0-41.41-generic\n
+            # server check-in: failed: livepatch check failed: POST request to \"https://livepatch.canonical.com/v1/client/e93452b8cff04bb8a6320ad7dae797e5/updates\" failed\n
+            # kernel state: ✓ kernel series 6.8 is covered by Livepatch\n
+            # patch state: ✓ no livepatches available for kernel 6.8.0-41.41-generic\n
+            # tier: updates (Free usage; This machine beta tests new patches.)\n
+            # machine id: e93452b8cff04bb8a6320ad7dae797e5",
 
     return data
