ecf57a989b0acd54629a424b3c5e91078711ff48
Justin <34551787+HJK-X@users.noreply.github.com>
2024-09-13 15:10:34 -0700

    fix: parse livepatch process status check (#266)

--- landscape/client/manager/livepatch.py
+++ landscape/client/manager/livepatch.py
@@ -4,7 +4,6 @@ import subprocess
 import traceback
 import yaml
 
-
 from landscape.client.manager.plugin import DataWatcherManager
 
 
@@ -22,10 +21,32 @@ class LivePatch(DataWatcherManager):
     run_interval = 1800  # Every 30 min
 
     def get_data(self):
-        json_output = get_livepatch_status('json')
-        readable_output = get_livepatch_status('humane')
-        return json.dumps({'humane': readable_output, 'json': json_output},
-                          sort_keys=True)  # Prevent randomness for cache
+        json_output = get_livepatch_status("json")
+        readable_output = get_livepatch_status("humane")
+        return json.dumps(
+            {"humane": readable_output, "json": json_output}, sort_keys=True
+        )  # Prevent randomness for cache
+
+
+def _parse_humane(output):
+    entries = output.splitlines()
+    data = {}
+
+    for entry in entries:
+        line = entry.strip()
+        if line == "":
+            continue
+        elif ":" in line:
+            key, value = line.split(":", 1)
+            key = key.strip().strip('"').strip("'")
+            value = value.strip().strip('"').strip("'")
+            data[key] = value
+        else:
+            raise yaml.YAMLError(
+                "Input is not a list of key/value pairs: " + output
+            )
+
+    return data
 
 
 def get_livepatch_status(format_type):
@@ -40,36 +61,39 @@ def get_livepatch_status(format_type):
     try:
         completed_process = subprocess.run(
             ["canonical-livepatch", "status", "--format", format_type],
-            encoding="utf8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            encoding="utf8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
     except FileNotFoundError as exc:
-        data['return_code'] = -1
-        data['error'] = str(exc)
-        data['output'] = ""
+        data["return_code"] = -1
+        data["error"] = str(exc)
+        data["output"] = ""
     except Exception as exc:
-        data['return_code'] = -2
-        data['error'] = str(exc)
-        data['output'] = ""
+        data["return_code"] = -2
+        data["error"] = str(exc)
+        data["output"] = ""
         logging.error(traceback.format_exc())
     else:
         output = completed_process.stdout.strip()
         try:
             if output:  # We don't want to parse an empty string
-                if format_type == 'json':
+                if format_type == "json":
                     output = json.loads(output)
-                    if 'Last-Check' in output:  # Remove timestamps for cache
-                        del output['Last-Check']
-                    if 'Uptime' in output:
-                        del output['Uptime']
+                    if "Last-Check" in output:  # Remove timestamps for cache
+                        del output["Last-Check"]
+                    if "Uptime" in output:
+                        del output["Uptime"]
                 else:
-                    output = yaml.safe_load(output)
-                    if 'last check' in output:
-                        del output['last check']
-            data['return_code'] = completed_process.returncode
-            data['error'] = completed_process.stderr
-            data['output'] = output
+                    output = _parse_humane(output)
+                    if "last check" in output:
+                        del output["last check"]
+            data["return_code"] = completed_process.returncode
+            data["error"] = completed_process.stderr
+            data["output"] = output
         except (yaml.YAMLError, json.decoder.JSONDecodeError) as exc:
-            data['return_code'] = completed_process.returncode
-            data['error'] = str(exc)
-            data['output'] = output
+            data["return_code"] = completed_process.returncode
+            data["error"] = str(exc)
+            data["output"] = output
 
     return data
