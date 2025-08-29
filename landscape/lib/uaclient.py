a7e3b964508e5bab00b064b59cf8d2f4c1fac833
David <david.mclain@canonical.com>
2025-08-29 15:27:37 -0500

    refactor: add wrapper for uaclient library calls (#358)

---
+++ landscape/lib/uaclient.py
@@ -0,0 +1,26 @@
+import logging
+
+from landscape.client import IS_CORE
+from landscape.client import IS_SNAP
+
+if not IS_SNAP and not IS_CORE:
+    from uaclient.status import status
+    from uaclient.config import UAConfig
+
+
+def get_pro_status():
+    """Calls uaclient.status to get pro information."""
+    try:
+        config = UAConfig()
+        pro_info = status(config)
+        return pro_info
+    except NameError:
+        logging.warning(
+            "Tried to use uaclient in SNAP or CORE environment, skipping call"
+        )
+        return {}
+    except Exception:
+        logging.warning(
+            "Could not get pro information for computer"
+        )
+        return {}
