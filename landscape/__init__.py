dd63196fe9bf542e26aeeb461c916d86c6c63132
abehnia <59957101+abehnia@users.noreply.github.com>
2025-10-17 13:29:01 -0700

    build: create 25.10~rc2 for jammy (#381)

--- landscape/__init__.py
+++ landscape/__init__.py
@@ -1,6 +1,6 @@
 DEBIAN_REVISION = "-0landscape0"
-UPSTREAM_VERSION = "25.10~rc1"
-PYTHON_VERSION = "25.10rc1"
+UPSTREAM_VERSION = "25.10~rc2"
+PYTHON_VERSION = "25.10rc2"
 VERSION = f"{UPSTREAM_VERSION}{DEBIAN_REVISION}"
 
 # The minimum server API version that all Landscape servers are known to speak
