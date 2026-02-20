6cfc7290250d1e4d3d93d3e886c0023590d714e7
Bill Kronholm <bill.kronholm@canonical.com>
2026-02-19 17:24:36 -0800

    tweaks for backwards compatibility

---
+++ landscape/client/__init__.py
@@ -0,0 +1,31 @@
+__all__ = [
+    "IS_CORE",
+    "IS_SNAP",
+    "USER",
+    "GROUP",
+    "UA_DATA_DIR",
+    "DEFAULT_CONFIG",
+]
+
+
+_DEPRECATION_MSG = (
+    "Direct import from 'landscape.client' is deprecated "
+    "and will be removed in landscape-client 28.0X. "
+    "Import from 'landscape.client.environment' instead."
+)
+
+
+def __getattr__(name):
+    if name in __all__:
+        import warnings
+
+        import landscape.client.environment as env
+
+        warnings.warn(_DEPRECATION_MSG, DeprecationWarning, stacklevel=2)
+        return getattr(env, name)
+
+    raise AttributeError(f"module 'landscape.client' has no attribute '{name}'")
+
+
+def __dir__():
+    return __all__
